1c64793d53694c4e1900f36ca5b138d90f9a4c10
Marcus Willock <crazcalm@gmail.com>
2017-05-30 18:55:41 -0400

    some formatting chances

--- test.go
+++ test.go
@@ -1,99 +1,108 @@
 package main
 
 import (
-	"time"
+	"flag"
 	"fmt"
+	"log"
 	"math"
 	"os"
 	"os/exec"
-	"flag"
-	"log"
 	"strings"
+	"time"
 )
 
-
+// Days Converts hours into days
 func Days(hours float64) (days float64) {
-       days = math.Floor(hours / 24)
-       return days
+	days = math.Floor(hours / 24)
+	return days
 }
 
-
+// TimeModX is used to convert time into its appropriate range
 func TimeModX(allHours, mod float64) (result float64) {
 	result = math.Floor(math.Mod(allHours, mod))
 	return
 }
 
-func Clear(){
+// Clear clears the screen.
+func Clear() {
 	c := exec.Command("clear")
 	c.Stdout = os.Stdout
 	c.Run()
 }
 
-func ValidYear(currentYear, futureYear int) (bool) {
+// ValidYear validates a year
+func ValidYear(currentYear, futureYear int) bool {
 	return currentYear <= futureYear
 }
 
-func ValidMonth(month int) (bool) {
+// ValidMonth validates a month
+func ValidMonth(month int) bool {
 	return 0 <= month && month <= 12
 }
 
+// ValidDay validates a day
 func ValidDay(lowerBound, upperBound, day int) bool {
 	return lowerBound <= day && day <= upperBound
 }
 
-func ValidHours(hours int)(bool){
+// ValidHours validates the hour used
+func ValidHours(hours int) bool {
 	return 0 <= hours && hours <= 23
 }
 
-func ValidMinutes(minutes int)(bool){
+// ValidMinutes validates the minutes used
+func ValidMinutes(minutes int) bool {
 	return 0 <= minutes && minutes <= 59
 }
 
+// ValidDate validates ensures that the used date is in the future
 func ValidDate(futureDate time.Time) (err error) {
 	now := time.Now()
-	if !now.Before(futureDate){
-		err = fmt.Errorf("Please choose a date in the future.")
+	if !now.Before(futureDate) {
+		err = fmt.Errorf("Please choose a date in the future")
 	}
 	return
 }
 
+// ValidateInput Checks user input
 func ValidateInput(year, month, day, hours, minutes, currentYear, lowerDayBound, upperDayBound int) (err error) {
 	if year == 0 && month == 0 && day == 0 {
 		err = fmt.Errorf("Please pass a date to be use for the countdown")
 		return
 	}
 
-	if !ValidYear(currentYear, year){
-		err = fmt.Errorf("year: %d is not in the future.", year)
+	if !ValidYear(currentYear, year) {
+		err = fmt.Errorf("year: %d is not in the future", year)
 		return
 	}
 
-	if !ValidMonth(month){
-		err = fmt.Errorf("%d is not a valid month. Try using the numbers 1 - 12.", month)
+	if !ValidMonth(month) {
+		err = fmt.Errorf("%d is not a valid month. Try using the numbers 1 - 12", month)
 		return
 	}
 
-	if !ValidDay(lowerDayBound, upperDayBound, day){
-		err = fmt.Errorf("%d is not a valid day for the selected month.", day)
+	if !ValidDay(lowerDayBound, upperDayBound, day) {
+		err = fmt.Errorf("%d is not a valid day for the selected month", day)
 		return
 	}
 
-	if !ValidHours(hours){
-		err = fmt.Errorf("%d is not a valid hour. Select an hour between 0 and 23.", hours)
+	if !ValidHours(hours) {
+		err = fmt.Errorf("%d is not a valid hour. Select an hour between 0 and 23", hours)
 		return
 	}
 
-	if !ValidMinutes(minutes){
-		err = fmt.Errorf("%d is not a valid minute. Select an minute between 0 and 59.", minutes)
+	if !ValidMinutes(minutes) {
+		err = fmt.Errorf("%d is not a valid minute. Select an minute between 0 and 59", minutes)
 		return
 	}
 	return
 }
 
-func FirstAndLastDayOfTheMonth(year, month int, location *time.Location)(first, last int){
+// FirstAndLastDayOfTheMonth finds the first and last day of the month
+func FirstAndLastDayOfTheMonth(year, month int, location *time.Location) (first, last int) {
 	date := time.Date(year, time.Month(month), 1, 0, 0, 0, 0, location)
 	first = date.Day()
-	last =date.AddDate(0, 1, -1).Day()
+	last = date.AddDate(0, 1, -1).Day()
 	return
 }
 
@@ -106,7 +115,7 @@ var minutes = flag.Int("minutes", 0, "Minutes")
 var refreshSeconds = flag.Int("refresh", 1, "Refresh time in seconds")
 var msg = flag.String("msg", "The magic day is almost here!", "The message that is displayed with the countdown")
 
-func main(){
+func main() {
 	flag.Parse()
 	loc, err := time.LoadLocation("Local")
 	if err != nil {
@@ -128,7 +137,7 @@ func main(){
 	if err != nil {
 		log.Fatalln(err)
 	}
-	
+
 	var diff time.Duration
 
 	for {
