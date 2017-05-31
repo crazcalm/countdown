4d919632b3fb7eb439a6bc6e24a56203e052f5df
Marcus Willock <crazcalm@gmail.com>
2017-05-30 21:39:18 -0400

    Fixing up the tests and formatting the code

--- countdown/countdown_test.go
+++ countdown/countdown_test.go
@@ -4,52 +4,52 @@ import (
 	"testing"
 )
 
-func TestDays(t *testing.T){
-	cases := []struct{
-			Hours 	float64
-			Answer 	float64
-		}{
-			{1,0},
-			{23,0},
-			{27,1},
-			{48,2},
-			{55,2},
+func TestDays(t *testing.T) {
+	cases := []struct {
+		Hours  float64
+		Answer float64
+	}{
+		{1, 0},
+		{23, 0},
+		{27, 1},
+		{48, 2},
+		{55, 2},
 	}
 
 	var result float64
 	for _, tests := range cases {
 		result = Days(tests.Hours)
 		if result != tests.Answer {
-			t.Errorf("Days(%.0f) == %.0f, but the answer is %.0f", tests.Hours, result, tests.Answer)
+			t.Errorf("Days(%.0f) = %.0f, but the answer should be %.0f", tests.Hours, result, tests.Answer)
 		}
 	}
 }
 
-func TestTimeModX(t *testing.T){
-	cases := []struct{
-		Time		float64
-		Mod			float64
-		Answer		float64
+func TestTimeModX(t *testing.T) {
+	cases := []struct {
+		Time   float64
+		Mod    float64
+		Answer float64
 	}{
-		{12,24,12},
-		{36,24, 12},
+		{12, 24, 12},
+		{36, 24, 12},
 		{66, 60, 6},
 	}
 
 	var result float64
-	for _, tests := range cases{
+	for _, tests := range cases {
 		result = TimeModX(tests.Time, tests.Mod)
 		if result != tests.Answer {
-			t.Errorf("TimeModX(%.0f) == %.0f, but the answer is %.0f", tests.Time, result, tests.Answer)
+			t.Errorf("TimeModX(%.0f) = %.0f, but the answer should be %.0f", tests.Time, result, tests.Answer)
 		}
 	}
 }
 
-func TestValidYear(t *testing.T){
-	cases := []struct{
-		CurrentYear		int
-		FutureYear		int
-		Answer			bool
+func TestValidYear(t *testing.T) {
+	cases := []struct {
+		CurrentYear int
+		FutureYear  int
+		Answer      bool
 	}{
 		{2017, 2017, true},
 		{2017, 2020, true},
@@ -57,18 +57,18 @@ func TestValidYear(t *testing.T){
 	}
 
 	var result bool
-	for _, tests := range cases{
+	for _, tests := range cases {
 		result = ValidYear(tests.CurrentYear, tests.FutureYear)
 		if result != tests.Answer {
-			t.Errorf("ValidYear(%d, %d) == %v, but the answer is %v", tests.CurrentYear, tests.FutureYear, result, tests.Answer)
+			t.Errorf("ValidYear(%d, %d) = %v, but the answer should be %v", tests.CurrentYear, tests.FutureYear, result, tests.Answer)
 		}
 	}
 }
 
-func TestValidMonth(t *testing.T){
-	cases := []struct{
-		Month	int
-		Answer	bool
+func TestValidMonth(t *testing.T) {
+	cases := []struct {
+		Month  int
+		Answer bool
 	}{
 		{0, false},
 		{1, true},
@@ -78,10 +78,33 @@ func TestValidMonth(t *testing.T){
 	}
 
 	var result bool
-	for _, tests := range cases{
+	for _, tests := range cases {
 		result = ValidMonth(tests.Month)
 		if result != tests.Answer {
-			t.Errorf("ValidMonth(%d) == %v, but the answer is %v", tests.Month, result, tests.Answer)
+			t.Errorf("ValidMonth(%d) = %v, but the answer should be %v", tests.Month, result, tests.Answer)
+		}
+	}
+}
+
+func TestValidDay(t *testing.T) {
+	cases := []struct {
+		LowerBound int
+		UpperBound int
+		Day        int
+		Answer     bool
+	}{
+		{1, 30, 0, false},
+		{1, 30, 1, true},
+		{1, 30, 15, true},
+		{1, 30, 30, true},
+		{1, 30, 31, false},
+	}
+
+	var result bool
+	for _, tests := range cases {
+		result = ValidDay(tests.LowerBound, tests.UpperBound, tests.Day)
+		if result != tests.Answer {
+			t.Errorf("ValidDay(%d, %d, %d) = %v, but the answer should be %b", tests.LowerBound, tests.UpperBound, tests.Day, result, tests.Answer)
 		}
 	}
 }
--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"github.com/crazcalm/countdown/countdown"
-	"fmt"
 	"flag"
-	"time"
+	"fmt"
+	"github.com/crazcalm/countdown/countdown"
 	"log"
 	"strings"
+	"time"
 )
 
 var year = flag.Int("year", 2020, "Year")
@@ -18,7 +18,7 @@ var minutes = flag.Int("minutes", 0, "Minutes")
 var refreshSeconds = flag.Int("refresh", 1, "Refresh time in seconds")
 var msg = flag.String("msg", "The magic day is almost here!", "The message that is displayed with the countdown")
 
-func main (){
+func main() {
 	fmt.Println("Hello World")
 
 	flag.Parse()
@@ -26,34 +26,34 @@ func main (){
 	if err != nil {
 		log.Fatal("Error with the location")
 	}
-	
+
 	now := time.Now()
 	currentYear, _, _ := now.Date()
 	dayLowerBound, dayUpperBound := countdown.FirstAndLastDayOfTheMonth(*year, *month, loc)
-	
+
 	err = countdown.ValidateInput(*year, *month, *day, *hours, *minutes, currentYear, dayLowerBound, dayUpperBound)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	
+
 	future := time.Date(*year, time.Month(*month), *day, *hours, *minutes, 0, 0, loc)
-	
+
 	err = countdown.ValidDate(future)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 	var diff time.Duration
-	
+
 	for {
 		countdown.Clear()
-	    diff = time.Until(future)
-	    if strings.Contains(diff.String(), "-") {
-	    	fmt.Printf("The countdown for '%s' is over!\n", *msg)
-	        break
-	    }
-	   fmt.Printf("%d-%d-%d: %s\n\n", *year, *month, *day, *msg)
-	   fmt.Printf("%.0fd %.0fh %.0fm %.0fs\n", countdown.Days(diff.Hours()), countdown.TimeModX(diff.Hours(), 24), countdown.TimeModX(diff.Minutes(), 60), countdown.TimeModX(diff.Seconds(), 60))
-	   time.Sleep(time.Duration(*refreshSeconds) * time.Second)
+		diff = time.Until(future)
+		if strings.Contains(diff.String(), "-") {
+			fmt.Printf("The countdown for '%s' is over!\n", *msg)
+			break
+		}
+		fmt.Printf("%d-%d-%d: %s\n\n", *year, *month, *day, *msg)
+		fmt.Printf("%.0fd %.0fh %.0fm %.0fs\n", countdown.Days(diff.Hours()), countdown.TimeModX(diff.Hours(), 24), countdown.TimeModX(diff.Minutes(), 60), countdown.TimeModX(diff.Seconds(), 60))
+		time.Sleep(time.Duration(*refreshSeconds) * time.Second)
 	}
 }
